48d87c002bbdd5964bd31018cac187f6b1d946e7
YutongDu <yutong.du@aalto.fi>
2025-01-11 14:11:16 +0200

    first commit and add postgreSQL

---
+++ flyway/sql/V1___initial_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    country_name TEXT NOT NULL,
+    land_price INTEGER DEFAULT 0,
+    house_price INTEGER DEFAULT 0,
+    collateral_value INTEGER DEFAULT 0,
+    empty_charge INTEGER DEFAULT 0,
+    one_house_charge INTEGER DEFAULT 0,
+    two_house_charge INTEGER DEFAULT 0,
+    hotel_charge INTEGER DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE properties_in_game (
+    id SERIAL PRIMARY KEY,
+    property_id INTEGER REFERENCES properties(id),
+    game_id TEXT NOT NULL,
+    house_number INTEGER DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP    
+);
+
+
+CREATE INDEX properties_id_idx ON properties (id);
+CREATE INDEX properties_in_game_game_id_idx ON properties_in_game (game_id);
---
+++ flyway/sql/V2___courses.sql
@@ -0,0 +1,33 @@
+INSERT INTO 
+properties 
+(
+    country_name, 
+    land_price, 
+    house_price, 
+    collateral_value,
+    empty_charge,
+    one_house_charge,
+    two_house_charge,
+    hotel_charge
+) VALUES 
+(
+    'China', /*country_name*/
+    1000, /*land_price*/
+    600, /*house_price*/
+    500, /*collateral_value*/
+    200, /*empty_charge*/
+    400, /*one_house_charge*/
+    600, /*two_house_charge*/
+    1000 /*hotel_charge*/
+    
+),
+(
+    'Swiss', /*country_name*/
+    3000, /*land_price*/
+    1000, /*house_price*/
+    1500, /*collateral_value*/
+    200, /*empty_charge*/
+    1800, /*one_house_charge*/
+    5000, /*two_house_charge*/
+    17000 /*hotel_charge*/
+)
